858af7d0759a94bb478510e461a61d19b7e3ac1c
Chelsea Quigley <20096536@mail.wit.ie>
2024-05-13 07:46:27 +0100

    Changed the layout on the movies page

--- reactApp/src/pages/moviesPage.js
+++ reactApp/src/pages/moviesPage.js
@@ -14,9 +14,17 @@ const MoviesPage = () => {
     const movies = data;
     const moviesDisplay = (
         <div>
-            {movies.map(movie => { return <li key={movie.id}>{movie.id},{movie.title}<br /></li> })}
+            <ul> 
+                {movies.map(movie => ( //adding movie id , title and overview in a list format.
+                    <li key={movie.id}>
+                        <h3>{movie.title}</h3>
+                        <p>{movie.overview}</p>
+                        <p>Release Date: {movie.release_date}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
     return <div><h2>Movies</h2>{moviesDisplay}</div>
 }
 
